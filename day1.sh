a7e130f00841bee5f73fa24401c0a3edae480f9e
Shivam Upadhyay <122978849+Shivmasaurr@users.noreply.github.com>
2025-06-25 18:40:16 +0530

    Create day1.sh
    day 1 part 1

---
+++ day1.sh
@@ -0,0 +1,5 @@
+date #returns date
+uptime #displays how long the system has been running and the average number of processes running over various periods of time.
+df #total free memory
+free #displays free memory
+exit
